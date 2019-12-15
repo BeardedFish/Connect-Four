9fc8afac89e54e7f9d217839c66ad8e83ed8b550
Darian Benam <darianbenam@hotmail.com>
2019-12-15 00:56:13 -0800

    Added in game result detection (winner, no winner, ongoing game)
    Also edited the graphics of the game

--- ConnectFour/Controls/ConnectFourGameContainer.cs
+++ ConnectFour/Controls/ConnectFourGameContainer.cs
@@ -10,6 +10,31 @@ namespace ConnectFour
         /// <summary>
         /// 
         /// </summary>
+        public enum Result
+        {
+            OngoingGame,
+            TiedGame,
+            RedPlayerWins,
+            YellowPlayerWins
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private enum Chip
+        {
+            Empty,
+            Red,
+            Yellow
+        }
+
+        public delegate void GameFinishedHandler(object sender, Result gameResult);
+        public event GameFinishedHandler OnGameFinished;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         private const int TOTAL_COLUMNS = 7;
 
         /// <summary>
@@ -31,28 +56,29 @@ namespace ConnectFour
 
         private int BOX_HEIGHT;
 
+        private bool gameOver = false;
+
         private bool redPlayerTurn = true;
+
+        private int redPlayerWinStreak = 0;
+        private int yellowPlayerWinStreak = 0;
 
         /// <summary>
         /// 
         /// </summary>
         private Chip[,] gameBoardArray = new Chip[TOTAL_ROWS, TOTAL_COLUMNS];
 
-        enum Chip
-        {
-            Empty,
-            Red,
-            Yellow
-        }
-
         public ConnectFourGameContainer()
         {
             this.DoubleBuffered = true;
+
+            BOX_WIDTH = (this.Width - GAME_GRID_X_OFFSET * 2) / TOTAL_COLUMNS;
+            BOX_HEIGHT = (this.Height - GAME_GRID_Y_OFFSET * 2) / TOTAL_ROWS;
         }
 
         public void StartNewGame()
         {
-
+            gameOver = false;
         }
 
         private void placeChipInColumn(int col, int row)
@@ -87,6 +113,132 @@ namespace ConnectFour
             }
 
             return -1;
+        }
+
+        private bool isGridFilled()
+        {
+            int sum = 0;
+
+            for (int row = 0; row < gameBoardArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameBoardArray.GetLength(1); col++)
+                {
+                    if (gameBoardArray[row, col] != Chip.Empty)
+                    {
+                        sum++;
+                    }
+                }
+            }
+
+            if (sum >= TOTAL_COLUMNS * TOTAL_ROWS)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private Result getGameResult()
+        {
+            for (int row = 0; row < gameBoardArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameBoardArray.GetLength(1) - 3; col++)
+                {
+                    if (gameBoardArray[row, col] == Chip.Red 
+                        && gameBoardArray[row, col + 1] == Chip.Red 
+                        && gameBoardArray[row, col + 2] == Chip.Red 
+                        && gameBoardArray[row, col + 3] == Chip.Red)
+                    {
+                        return Result.RedPlayerWins;
+                    }
+
+                    if (gameBoardArray[row, col] == Chip.Yellow
+                         && gameBoardArray[row, col + 1] == Chip.Yellow
+                         && gameBoardArray[row, col + 2] == Chip.Yellow
+                         && gameBoardArray[row, col + 3] == Chip.Yellow)
+                    {
+                        return Result.YellowPlayerWins;
+                    }
+
+                }
+            }
+
+            for (int row = 0; row < gameBoardArray.GetLength(0) - 3; row++)
+            {
+                for (int col = 0; col < gameBoardArray.GetLength(1); col++)
+                {
+                    if (gameBoardArray[row, col] == Chip.Red
+                        && gameBoardArray[row + 1, col] == Chip.Red
+                        && gameBoardArray[row + 2, col] == Chip.Red
+                        && gameBoardArray[row + 3, col] == Chip.Red)
+                    {
+                        return Result.RedPlayerWins;
+                    }
+
+                    if (gameBoardArray[row, col] == Chip.Yellow
+                        && gameBoardArray[row + 1, col] == Chip.Yellow
+                        && gameBoardArray[row + 2, col] == Chip.Yellow
+                        && gameBoardArray[row + 3, col] == Chip.Yellow)
+                    {
+                        return Result.YellowPlayerWins;
+                    }
+                }
+            }
+
+            for (int row = 0; row < gameBoardArray.GetLength(0) - 3; row++)
+            {
+                for (int col = 0; col < gameBoardArray.GetLength(1) - 3; col++)
+                {
+                    if (gameBoardArray[row, col] == Chip.Red
+                        && gameBoardArray[row + 1, col + 1] == Chip.Red
+                        && gameBoardArray[row + 2, col + 2] == Chip.Red
+                        && gameBoardArray[row + 3, col + 3] == Chip.Red)
+                    {
+                        return Result.RedPlayerWins;
+                    }
+
+                    if (gameBoardArray[row, col] == Chip.Yellow
+                        && gameBoardArray[row + 1, col + 1] == Chip.Yellow
+                        && gameBoardArray[row + 2, col + 2] == Chip.Yellow
+                        && gameBoardArray[row + 3, col + 3] == Chip.Yellow)
+                    {
+                        return Result.YellowPlayerWins;
+                    }
+                }
+            }
+
+            for (int row = 0; row < gameBoardArray.GetLength(0) - 3; row++)
+            {
+                for (int col = gameBoardArray.GetLength(1) - 1; col >= 3; col--)
+                {
+                    if (gameBoardArray[row, col] == Chip.Red
+                        && gameBoardArray[row + 1, col - 1] == Chip.Red
+                        && gameBoardArray[row + 2, col - 2] == Chip.Red
+                        && gameBoardArray[row + 3, col - 3] == Chip.Red)
+                    {
+                        return Result.RedPlayerWins;
+                    }
+
+                    if (gameBoardArray[row, col] == Chip.Yellow
+                        && gameBoardArray[row + 1, col - 1] == Chip.Yellow
+                        && gameBoardArray[row + 2, col - 2] == Chip.Yellow
+                        && gameBoardArray[row + 3, col - 3] == Chip.Yellow)
+                    {
+                        return Result.YellowPlayerWins;
+                    }
+                }
+            }
+
+            if (isGridFilled())
+            {
+                return Result.TiedGame;
+            }
+
+            return Result.OngoingGame;
         }
 
         private int hoveredColumn;
@@ -129,17 +281,51 @@ namespace ConnectFour
         /// <param name="e"></param>
         protected override void OnMouseClick(MouseEventArgs e)
         {
-            //MessageBox.Show(hoveredColumn.ToString());
+            if (gameOver)
+            {
+                return;
+            }
+
             int row = getRowToPlaceChip(hoveredColumn);
             if (row != -1)
             {
                 placeChipInColumn(hoveredColumn, row);
 
-                redPlayerTurn = !redPlayerTurn;
-             
+                Result gameResult = getGameResult();
+                switch (gameResult)
+                {
+                    case Result.OngoingGame:
+                        redPlayerTurn = !redPlayerTurn;
+                        break;
+                    case Result.RedPlayerWins:
+                        redPlayerWinStreak++;
+                        break;
+                    case Result.TiedGame:
 
+                        break;
+                    case Result.YellowPlayerWins:
+                        yellowPlayerWinStreak++;
+                        break;
+                }
+
+                if (gameResult != Result.OngoingGame)
+                {
+                    if (OnGameFinished != null)
+                    {
+                        OnGameFinished(this, gameResult);
+                    }
+
+                    gameOver = true;
+                }
             }
+        }
 
+        protected override void OnResize(EventArgs e)
+        {
+
+            BOX_WIDTH = (this.Width - GAME_GRID_X_OFFSET * 2) / TOTAL_COLUMNS;
+            BOX_HEIGHT = (this.Height - GAME_GRID_Y_OFFSET * 2) / TOTAL_ROWS;
+            this.Invalidate();
         }
 
         /// <summary>
@@ -152,14 +338,17 @@ namespace ConnectFour
         /// <param name="borderThickness"></param>
         private void drawChip(Graphics g, Chip chipType, int col, int row, int borderThickness)
         {
-            int chipHeight = (int)(BOX_HEIGHT * 0.75);
-            int chipWidth = (int)(BOX_WIDTH * 0.75);
-            int chipOffset = 10;
+            int chipOffset = 15;
 
-            Rectangle chipRect = new Rectangle(GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
+           /* Rectangle chipRect = new Rectangle(GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
                 GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
-                chipWidth - chipOffset + borderThickness,
-                chipHeight - chipOffset + borderThickness);
+                chipWidth - chipOffset  + borderThickness,
+                chipHeight - chipOffset + borderThickness);*/
+
+            Rectangle chipRect = new Rectangle(GAME_GRID_X_OFFSET + (col * BOX_WIDTH) + chipOffset,
+                            GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
+                            BOX_WIDTH - (chipOffset * 2) + borderThickness,
+                            BOX_HEIGHT - (chipOffset * 2) + borderThickness);
 
             switch (chipType)
             {
@@ -167,43 +356,38 @@ namespace ConnectFour
                 case Chip.Empty:
                     using (var br = new LinearGradientBrush(chipRect, Color.FromArgb(255, 255, 255), Color.FromArgb(215, 215, 215), 90))
                     {
-                        g.FillEllipse(br,
+                        /*g.FillEllipse(br,
                             GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
                             GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
                             chipWidth - chipOffset + borderThickness,
-                            chipHeight - chipOffset + borderThickness);
+                            chipHeight - chipOffset + borderThickness );
+                        */
+
+                        g.FillEllipse(br, chipRect);
                     }
                     break;
                 case Chip.Red:
                     using (var br = new LinearGradientBrush(chipRect, Color.FromArgb(245, 0, 0), Color.FromArgb(155, 0, 0), 90))
                     {
-                        g.FillEllipse(br,
-                            GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
-                            GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
-                            chipWidth - chipOffset + borderThickness,
-                            chipHeight - chipOffset + borderThickness);
+                        g.FillEllipse(br, chipRect);
                     }
                     break;
                 case Chip.Yellow:
                     using (var br = new LinearGradientBrush(chipRect, Color.FromArgb(227, 229, 123), Color.FromArgb(203, 197, 49), 90))
                     {
-                        g.FillEllipse(br,
-                            GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
-                            GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
-                            chipWidth - chipOffset + borderThickness,
-                            chipHeight - chipOffset + borderThickness);
+                        g.FillEllipse(br, chipRect);
                     }
                     break;
             }
 
             using (var pen = new Pen(Color.Black, borderThickness))
             {
-                g.DrawEllipse(pen, GAME_GRID_X_OFFSET + chipOffset + (col * BOX_WIDTH),
-                    GAME_GRID_Y_OFFSET + row * BOX_HEIGHT + chipOffset,
-                    chipWidth - chipOffset + borderThickness,
-                    chipHeight - chipOffset + borderThickness);
+                g.DrawEllipse(pen, chipRect);
             }
         }
+
+
+
 
         /// <summary>
         /// 
@@ -220,10 +404,26 @@ namespace ConnectFour
                 g.FillRectangle(br, this.ClientRectangle);
             }
 
+            using (var br = new LinearGradientBrush(this.ClientRectangle, Color.FromArgb(126, 85, 63), Color.FromArgb(64, 42, 29), 90))
+            {
+                Point[] floorPointsArray =
+                {
+                    new Point(0, this.Height),
+                    new Point(GAME_GRID_X_OFFSET - (BOX_WIDTH / 2), this.Height - GAME_GRID_Y_OFFSET - (BOX_HEIGHT / 2)),
+                    new Point(this.Width - GAME_GRID_X_OFFSET + (BOX_WIDTH / 2), this.Height - GAME_GRID_Y_OFFSET - (BOX_HEIGHT / 2)),
+                    new Point(this.Width, this.Height)
+                };
+
+                g.FillPolygon(br, floorPointsArray);
+                using (var p = new Pen(Color.Black, 2))
+                {
+                    g.DrawPolygon(p, floorPointsArray);
+                }
+            }
+
+
             Rectangle gameRect = new Rectangle(GAME_GRID_X_OFFSET, GAME_GRID_Y_OFFSET, this.Width - GAME_GRID_X_OFFSET * 2 - 2, this.Height - GAME_GRID_Y_OFFSET * 2 - 2);
-
-
-            using (var br = new LinearGradientBrush(this.ClientRectangle, Color.FromArgb(64, 138, 196), Color.FromArgb(18, 82, 129), 90))
+            using (var br = new LinearGradientBrush(gameRect, Color.FromArgb(64, 138, 196), Color.FromArgb(18, 82, 129), 90))
             {
                 g.FillRectangle(br, gameRect);
             }
@@ -231,24 +431,19 @@ namespace ConnectFour
             int xOffset = 175;
             int yOffset = 75;
 
-            int boxHeight = (this.Height - GAME_GRID_Y_OFFSET * 2) / TOTAL_ROWS;
-            int boxWidth = ((this.Width - GAME_GRID_X_OFFSET * 2)) / TOTAL_COLUMNS;
-
-            BOX_HEIGHT = boxHeight;
-            BOX_WIDTH = boxWidth;
-
+ 
             int lineThickness = 2;
 
             using (var pen = new Pen(Color.Black, lineThickness))
             {
                 for (int i = 0; i <= TOTAL_ROWS; i++)
                 {
-                    g.DrawLine(pen, xOffset, yOffset + (i * boxHeight), this.Width - xOffset, yOffset + (i * boxHeight));
+                    g.DrawLine(pen, xOffset, yOffset + (i * BOX_HEIGHT), this.Width - xOffset, yOffset + (i * BOX_HEIGHT));
                 }
 
                 for (int i = 0; i <= TOTAL_COLUMNS; i++)
                 {
-                    g.DrawLine(pen, xOffset + (i * boxWidth), yOffset, xOffset + (i * boxWidth), yOffset + (boxHeight * TOTAL_ROWS));
+                    g.DrawLine(pen, xOffset + (i * BOX_WIDTH), yOffset, xOffset + (i * BOX_WIDTH), yOffset + (BOX_HEIGHT * TOTAL_ROWS));
                 }
             }
 
@@ -261,9 +456,11 @@ namespace ConnectFour
                 }
             }
 
-            Chip hoverChipType = redPlayerTurn ? Chip.Red : Chip.Yellow;
-
-            drawChip(g, hoverChipType, hoveredColumn, -1, 2);
+            if (!gameOver)
+            {
+                Chip hoverChipType = redPlayerTurn ? Chip.Red : Chip.Yellow;
+                drawChip(g, hoverChipType, hoveredColumn, -1, 2);
+            }
 
             g.DrawString("Connect Four - Version 1.0 | By: Darian Benam", this.Font, Brushes.White, 5, 5);
         }
