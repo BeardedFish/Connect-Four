f151dc01c005bab91bfb049b4011bc504a83c555
Darian Benam <darianbenam@hotmail.com>
2019-12-17 16:44:51 -0800

    Updated 'AssemblyInfo.cs' file with more information about the assembly

--- ConnectFour/Properties/AssemblyInfo.cs
+++ ConnectFour/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConnectFour")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Connect Four game designed for Microsoft Windows operating system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Darian Benam")]
 [assembly: AssemblyProduct("ConnectFour")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("N/A")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
